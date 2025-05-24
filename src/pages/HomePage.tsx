4e958d1a1b84d287c204fdcf77e1c5a748b821d1
蒲泽辉 <15573613+pu-zehui@user.noreply.gitee.com>
2025-05-24 18:03:45 +0800

    尝试修复svg

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -129,7 +129,7 @@ const HomePage = () => {
             
             <div className="flex justify-center">
               <img 
-                src="/how to learn.svg" 
+                src="/public/how-to-learn.svg" 
                 alt="Learning Process" 
                 className="max-w-full h-auto rounded-lg dark:invert"
               />
